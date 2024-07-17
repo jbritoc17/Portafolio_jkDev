6a9a6c11a53235099dea849167f0d406b545007b
jbritoc17 <jbritoc17@outlook.es>
2024-07-16 20:13:27 -0500

    agregando carrusel

--- ESTUDIAR/REACT/Portafolio/src/components/MyProjects/Projects.jsx
+++ ESTUDIAR/REACT/Portafolio/src/components/MyProjects/Projects.jsx
@@ -1,27 +1,22 @@
-
 import "./Projects.css";
 import { PROJECTS } from "../../utils/data";
 /* import ProjectsCard from "./ProjectsCard/ProjectsCard"; */
 
 const Projects = () => {
-
-  
   return (
     <section className="projects-container">
       <h5>Projects</h5>
 
       <div className="projects-content">
-      {PROJECTS.map(item=>(
-        
-        
-          (<div className="projects-carrusel" key={item.tittle}>
-          
-            <a className="carrusel-img" href=""> <img src={item.image} alt={item.tittle} /></a>
-            
+        {PROJECTS.map((item) => (
+          <div className="projects-carrusel" key={item.tittle}>
+            <a className="carrusel-img" href="">
+              <img src={item.image} alt={item.tittle} />
+            </a>
+
             <span>{item.skills}</span>
-          </div>)
-        
-      ))}
+          </div>
+        ))}
       </div>
     </section>
   );
